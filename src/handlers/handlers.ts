626d97ae9d89fa61f1be42d5301ea0f7146b0262
gbrlcoelho <coelhogabriel67@gmail.com>
2024-03-26 19:43:54 -0300

    feat(handlers): updated sub-module handler

--- src/handlers/handlers.ts
+++ src/handlers/handlers.ts
@@ -2,7 +2,7 @@ import {SelectType} from '../prompt/prompt.js';
 
 import {createComponent} from './component/create.js';
 import {createHook} from './hook/create.js';
-import {createSubModule} from './module/create.js';
+import {createSubModule} from './sub-module/create.js';
 
 export const handlers = (
   subcommand: string,
